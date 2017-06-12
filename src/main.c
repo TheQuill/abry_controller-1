c2a5e792b1ad22ea2a1878460170918e3827b1d8
Erik Rakhorst <erik.rakhorst@gmail.com>
2017-06-12 22:54:18 +0200

    tim1 channel 2 naar high polarity

--- src/main.c
+++ src/main.c
@@ -245,13 +245,11 @@ static void MX_TIM1_Init(void)
     Error_Handler();
   }
 
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_LOW;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_2) != HAL_OK)
   {
     Error_Handler();
   }
 
-  sConfigOC.OCNPolarity = TIM_OCNPOLARITY_HIGH;
   if (HAL_TIM_PWM_ConfigChannel(&htim1, &sConfigOC, TIM_CHANNEL_3) != HAL_OK)
   {
     Error_Handler();
